9d6e467dc6e7151346eb4645726c0c6f57233003
Lotta Rydin <lotta.rydin@telia.com>
2021-04-28 15:48:53 +0200

    htseq code

---
+++ code/9_htseq_BH_differential_analysis_code.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -l
+
+#SBATCH -A g2021012
+#SBATCH -M snowy
+#SBATCH -p core
+#SBATCH -n 1
+#SBATCH -t 10:00:00
+#SBATCH -J htseq_BH_differential_analysis_job
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user lotta.rydin@telia.com
+
+# Load modules:
+module load bioinfo-tools
+module load samtools
+module load htseq
+
+# commands:
+for f in ~/GenomeAnalysis_project/analyses/8_bwa_rna_align/sampe_BH/aln*
+    samtools index $f ${f}.bai
+done
+
+htseq-count -f bam -r pos -o ~/GenomeAnalysis_project/analyses/9_htseq_differential_analysis/BH_htseq_output.bam\
+~/GenomeAnalysis_project/analyses/8_bwa_rna_align/sampe_BH/aln* \
+~/GenomeAnalysis_project/analyses/5_prokka_annotation/annotation_trimmed.gff \
+
